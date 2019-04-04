fc4b1b9ec86b41edf624aca6349e4f2748503883
cefernandez <cefernandez@vectoritcgroup.com>
2019-04-04 13:01:25 +0200

    added swagger for rest services

--- src/main/java/com/eladio/pedidos/app/DemoApplication.java
+++ src/main/java/com/eladio/pedidos/app/DemoApplication.java
@@ -1,15 +1,19 @@
 package com.eladio.pedidos.app;
 
+import com.eladio.pedidos.config.SwaggerConfig;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan({"com.eladio.pedidos.model"})
 @EnableJpaRepositories({"com.eladio.pedidos.repository"})
 @ComponentScan({"com.eladio.pedidos.service", "com.eladio.pedidos.rest"})
+@Import(SwaggerConfig.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
